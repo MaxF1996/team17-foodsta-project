8f84d076590e345e42a1d7cf86a6f87e993eed9a
MaxF1996 <maxeffect.apollo@gmail.com>
2022-11-02 23:41:53 +0200

    cleanin js and sass folder

--- src/index.js
+++ src/index.js
@@ -10,19 +10,8 @@ import './modal';
     openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
 
     mobileMenu.classList.toggle('is-open');
-    const scrollLockMethod = !isMenuOpen ? 'disableBodyScroll' : 'enableBodyScroll';
-    bodyScrollLock[scrollLockMethod](document.body);
   };
 
   closeMenuBtn.addEventListener('click', toggleMenu);
   openMenuBtn.addEventListener('click', toggleMenu);
-
-  // Close the mobile menu on wider screens if the device orientation changes
-
-  window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
-    if (!e.matches) return;
-    mobileMenu.classList.remove('is-open');
-    openMenuBtn.setAttribute('aria-expanded', false);
-    bodyScrollLock.enableBodyScroll(document.body);
-  });
 })();
